2fae7935442acdc8a754305c57e964a2e844a884
Dhruv Patel <132053104+drp004@users.noreply.github.com>
2025-01-22 23:21:05 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+
+
+// Import required items
+const authRoute = require("./routes/authRoutes");
+const addExpense = require("./routes/Expense");
+const Income = require("./routes/Income");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.set("view engine", "ejs");
+app.set("views", "../client");
+
+
+mongoose.connect("mongodb://localhost:27017/Expense_tracker").then(() => console.log("db Connected!")).catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.use("/auth", authRoute);
+app.use("/Expense", addExpense);
+app.use("/Income", Income);
+
+
+app.listen(4500, () => {
+    console.log(`Server run on: http://localhost:${4500}`);
+});
